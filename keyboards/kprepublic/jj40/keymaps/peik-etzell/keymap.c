eae089b142179a22b5d7263afd4ec2e79cdbac1b
Peik Etzell <peik.etzell@gmail.com>
2025-03-07 08:18:23 +0200

    Use enum in jj40 keymap

--- keyboards/kprepublic/jj40/keymaps/peik-etzell/keymap.c
+++ keyboards/kprepublic/jj40/keymaps/peik-etzell/keymap.c
@@ -6,22 +6,24 @@
  * edit it directly.
  */
 
+enum layers { _QWERTY, _ARROWS, _NUMBERS, _FUNCTIONS, _META };
+
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-//    ┌────────────┬───────┬──────┬──────┬───────┬──────┬─────┬───────┬──────┬──────┬────┬─────────────┐
-//    │    tab     │   q   │  w   │  e   │   r   │  t   │  y  │   u   │  i   │  o   │ p  │    bspc     │
-//    ├────────────┼───────┼──────┼──────┼───────┼──────┼─────┼───────┼──────┼──────┼────┼─────────────┤
-//    │ LT(1, esc) │   a   │  s   │  d   │   f   │  g   │  h  │   j   │  k   │  l   │ ;  │      '      │
-//    ├────────────┼───────┼──────┼──────┼───────┼──────┼─────┼───────┼──────┼──────┼────┼─────────────┤
-//    │    lsft    │   z   │  x   │  c   │   v   │  b   │  n  │   m   │  ,   │  .   │ /  │ RSFT_T(ent) │
-//    ├────────────┼───────┼──────┼──────┼───────┼──────┼─────┼───────┼──────┼──────┼────┼─────────────┤
-//    │    lctl    │ MO(4) │ lgui │ lalt │ MO(2) │ lsft │ spc │ MO(3) │ left │ down │ up │    rght     │
-//    └────────────┴───────┴──────┴──────┴───────┴──────┴─────┴───────┴──────┴──────┴────┴─────────────┘
-[0] = LAYOUT_ortho_4x12(
-  KC_TAB        , KC_Q  , KC_W    , KC_E    , KC_R  , KC_T    , KC_Y   , KC_U  , KC_I    , KC_O    , KC_P    , KC_BSPC       ,
-  LT(1, KC_ESC) , KC_A  , KC_S    , KC_D    , KC_F  , KC_G    , KC_H   , KC_J  , KC_K    , KC_L    , KC_SCLN , KC_QUOT       ,
-  KC_LSFT       , KC_Z  , KC_X    , KC_C    , KC_V  , KC_B    , KC_N   , KC_M  , KC_COMM , KC_DOT  , KC_SLSH , RSFT_T(KC_ENT),
-  KC_LCTL       , MO(4) , KC_LGUI , KC_LALT , MO(2) , KC_LSFT , KC_SPC , MO(3) , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT
+//    ┌──────────────────┬───────────┬──────┬──────┬──────────────┬──────┬─────┬────────────────┬──────┬──────┬────┬─────────────┐
+//    │       tab        │     q     │  w   │  e   │      r       │  t   │  y  │       u        │  i   │  o   │ p  │    bspc     │
+//    ├──────────────────┼───────────┼──────┼──────┼──────────────┼──────┼─────┼────────────────┼──────┼──────┼────┼─────────────┤
+//    │ LT(_ARROWS, esc) │     a     │  s   │  d   │      f       │  g   │  h  │       j        │  k   │  l   │ ;  │      '      │
+//    ├──────────────────┼───────────┼──────┼──────┼──────────────┼──────┼─────┼────────────────┼──────┼──────┼────┼─────────────┤
+//    │       lsft       │     z     │  x   │  c   │      v       │  b   │  n  │       m        │  ,   │  .   │ /  │ RSFT_T(ent) │
+//    ├──────────────────┼───────────┼──────┼──────┼──────────────┼──────┼─────┼────────────────┼──────┼──────┼────┼─────────────┤
+//    │       lctl       │ MO(_META) │ lgui │ lalt │ MO(_NUMBERS) │ lsft │ spc │ MO(_FUNCTIONS) │ left │ down │ up │    rght     │
+//    └──────────────────┴───────────┴──────┴──────┴──────────────┴──────┴─────┴────────────────┴──────┴──────┴────┴─────────────┘
+[_QWERTY] = LAYOUT_ortho_4x12(
+  KC_TAB              , KC_Q      , KC_W    , KC_E    , KC_R         , KC_T    , KC_Y   , KC_U           , KC_I    , KC_O    , KC_P    , KC_BSPC       ,
+  LT(_ARROWS, KC_ESC) , KC_A      , KC_S    , KC_D    , KC_F         , KC_G    , KC_H   , KC_J           , KC_K    , KC_L    , KC_SCLN , KC_QUOT       ,
+  KC_LSFT             , KC_Z      , KC_X    , KC_C    , KC_V         , KC_B    , KC_N   , KC_M           , KC_COMM , KC_DOT  , KC_SLSH , RSFT_T(KC_ENT),
+  KC_LCTL             , MO(_META) , KC_LGUI , KC_LALT , MO(_NUMBERS) , KC_LSFT , KC_SPC , MO(_FUNCTIONS) , KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT
 ),
 
 //    ┌──────┬─────────┬─────────┬─────────┬─────────┬─────────┬─────────┬─────────┬──────┬──────┬────┬────────────┐
@@ -33,7 +35,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    ├──────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼─────────┼──────┼──────┼────┼────────────┤
 //    │ lctl │   no    │  lgui   │  lalt   │   no    │  lsft   │  lsft   │   no    │  no  │  no  │ no │     no     │
 //    └──────┴─────────┴─────────┴─────────┴─────────┴─────────┴─────────┴─────────┴──────┴──────┴────┴────────────┘
-[1] = LAYOUT_ortho_4x12(
+[_ARROWS] = LAYOUT_ortho_4x12(
   KC_NO   , LGUI(KC_1) , LGUI(KC_2) , LGUI(KC_3) , LGUI(KC_4) , LGUI(KC_5) , LGUI(KC_6) , LGUI(KC_7) , KC_HOME , KC_END  , KC_NO , LCTL(KC_BSPC),
   KC_TRNS , KC_NO      , KC_DEL     , KC_NO      , KC_NO      , KC_NO      , KC_LEFT    , KC_DOWN    , KC_UP   , KC_RGHT , KC_NO , KC_NO        ,
   KC_LSFT , KC_NO      , KC_NO      , KC_NO      , KC_NO      , KC_NO      , KC_NO      , KC_NO      , KC_NO   , KC_NO   , KC_NO , KC_LSFT      ,
@@ -49,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    ├──────┼────┼──────┼──────┼─────┼──────┼─────┼────┼────┼────┼────┼──────┤
 //    │ lctl │ no │ lgui │ lalt │     │ lsft │ spc │ no │ no │ no │ no │  no  │
 //    └──────┴────┴──────┴──────┴─────┴──────┴─────┴────┴────┴────┴────┴──────┘
-[2] = LAYOUT_ortho_4x12(
+[_NUMBERS] = LAYOUT_ortho_4x12(
   KC_TILD , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_BSPC,
   KC_NO   , KC_EXLM , KC_AT   , KC_HASH , KC_DLR  , KC_PERC , KC_CIRC , KC_ASTR , KC_MINS , KC_PLUS , KC_NO   , KC_GRV ,
   KC_LSFT , KC_BSLS , KC_AMPR , KC_PIPE , KC_NO   , KC_NO   , KC_NO   , KC_UNDS , KC_COMM , KC_DOT  , KC_SLSH , KC_EQL ,
@@ -65,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    ├──────┼────┼──────┼──────┼─────┼──────┼─────┼─────┼────┼────┼────┼──────┤
 //    │ lctl │ no │ lgui │ lalt │ no  │ lsft │ spc │     │ no │ no │ no │  no  │
 //    └──────┴────┴──────┴──────┴─────┴──────┴─────┴─────┴────┴────┴────┴──────┘
-[3] = LAYOUT_ortho_4x12(
+[_FUNCTIONS] = LAYOUT_ortho_4x12(
   KC_TILD , KC_F1 , KC_F2   , KC_F3   , KC_F4  , KC_NO   , KC_NO  , KC_NO   , KC_LPRN , KC_RPRN , KC_NO , KC_NO  ,
   KC_NO   , KC_F5 , KC_F6   , KC_F7   , KC_F8  , KC_NO   , KC_NO  , KC_NO   , KC_LCBR , KC_RCBR , KC_NO , KC_GRV ,
   KC_LSFT , KC_F9 , KC_F10  , KC_F11  , KC_F12 , KC_NO   , KC_NO  , KC_NO   , KC_LBRC , KC_RBRC , KC_NO , KC_LSFT,
@@ -81,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    ├────┼─────┼────┼────┼────┼────┼────┼────┼──────────┼─────────┼──────┼─────────┤
 //    │ no │     │ no │ no │ no │ no │ no │ no │    no    │  vold   │ volu │   no    │
 //    └────┴─────┴────┴────┴────┴────┴────┴────┴──────────┴─────────┴──────┴─────────┘
-[4] = LAYOUT_ortho_4x12(
+[_META] = LAYOUT_ortho_4x12(
   KC_NO , KC_NO   , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO    , KC_NO   , KC_NO   , RGB_TOG,
   KC_NO , KC_NO   , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , RGB_RMOD , RGB_MOD , KC_NO   , KC_NO  ,
   KC_NO , KC_NO   , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO , KC_NO    , KC_NO   , KC_NO   , KC_NO  ,
